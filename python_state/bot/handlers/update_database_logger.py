269a90652056ca74ba18e0d9cd24c4de34e694aa
ChastovSlava <crychs142@gmail.com>
2025-11-01 18:47:02 +0300

    add my third homework

---
+++ python_state/bot/handlers/update_database_logger.py
@@ -0,0 +1,10 @@
+from bot.handlers.handler import Handler, HandlerStatus
+import bot.database_client
+
+class DatabaseLogger(Handler):
+    def can_handle(self, update: dict, state: str, order_json: dict) -> bool:
+        return True
+    
+    def handle(self, update: dict, state: str, order_json: dict) -> HandlerStatus:
+        bot.database_client.persist_update([update])
+        return HandlerStatus.CONTINUE
